1323d659fe3b7123ae215fee10464f5e6718795d
Pabl1k <poiumazaya@gmail.com>
2021-07-25 22:12:26 +0300

    restore password component

--- src/testComponents/TestComponents.tsx
+++ src/testComponents/TestComponents.tsx
@@ -2,6 +2,8 @@ import React, {useState} from "react"
 import {AddItemForm} from "../components/common/addItemForm/AddItemForm"
 import {Checkbox} from "../components/common/checkbox/Checkbox"
 import style from "./TestComponents.module.scss"
+import {Button} from "../components/common/button/Button";
+import {InputText} from "../components/common/inputText/InputText";
 
 type TestComponentsPropsType = {}
 
@@ -27,6 +29,9 @@ export const TestComponents = React.memo(function (props: TestComponentsPropsTyp
             >
                 Checkbox text
             </Checkbox>
+
+            <InputText style={{width:150, height: 50}} placeholder={'input'}/>
+            <Button style={{width:150, height: 50}}>Button</Button>
         </>
     )
 })
