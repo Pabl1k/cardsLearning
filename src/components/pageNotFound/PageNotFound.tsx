5aff7c7c9e24fcdd0cdf794b38634926b5abe9fd
knakna <kna@tut.by>
2021-07-31 18:48:22 +0300

    change styles to page 404

--- src/components/pageNotFound/PageNotFound.tsx
+++ src/components/pageNotFound/PageNotFound.tsx
@@ -1,22 +1,22 @@
 import React from "react"
 import {NavLink} from "react-router-dom"
-import search from "../../assets/images/search.jpg"
+import search from "../../assets/images/search.svg"
 import s from "./PageNotFound.module.scss"
 
 export const PageNotFound = React.memo(() => {
     return (
         <div className={s.pageNotFoundBlock}>
-            <div className={s.container}>
-                <div className={s.inner}>
+
+
                     <h1 className={s.number}> 404</h1>
                     <h2 className={s.error}>Page Not Found!</h2>
-                    <p className={s.text}>MOST LIKELY THE PAGE YOU ARE LOOKING FOR THERE</p>
                     <div className={s.wrapIcon}>
                         <img className={s.search} src={search} alt="icon"/>
                     </div>
-                    <NavLink to="/" className={s.link}>Back to home</NavLink>
-                </div>
-            </div>
+                    <p className={s.text}>MOST LIKELY THE PAGE <br/>YOU ARE LOOKING FOR THERE</p>
+                    <NavLink to="/" className={s.linkHome}>Back to home</NavLink>
+
+
         </div>
     )
 })
