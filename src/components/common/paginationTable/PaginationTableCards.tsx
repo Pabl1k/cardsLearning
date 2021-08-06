cbc19431ccd2c81164a564223f71032dba41142f
PavlinMavlin <korobovpavel2017@yandex.by>
2021-08-06 13:17:59 +0300

    added PaginationTableCards

---
+++ src/components/common/paginationTable/PaginationTableCards.tsx
@@ -0,0 +1,53 @@
+import React, {ChangeEvent} from "react"
+import {Pagination} from "@material-ui/lab"
+import {Typography, useMediaQuery, useTheme} from "@material-ui/core"
+import s from "./PaginationTable.module.scss"
+import {useStyles} from "./PaginationTableStyles"
+import {ShowValueType} from "../../packsList/PacksList";
+
+type PaginationTablePropsType = {
+    item: number
+    setItem: (value: number) => void
+    setPerPage: (value: ShowValueType) => void
+    count?: number
+}
+
+export const PaginationTableCards = React.memo((props: PaginationTablePropsType) => {
+
+    const classes = useStyles()
+    const theme = useTheme()
+    const isPhoneSize = useMediaQuery(theme.breakpoints.down("xs"))
+
+    const onPageChangeHandler = (event: ChangeEvent<unknown>, value: number) => {
+        props.setItem(value)
+    }
+    const onCardsPerPageChangeHandler = (e: ChangeEvent<any>) => {
+        props.setPerPage(e.currentTarget.value)
+    }
+    return (
+        <div className={classes.root}>
+            <div className={s.pagination}>
+                <div className={s.paginationMUI}>
+                    <Typography>Page: {props.item}</Typography>
+                    {/*{isPhoneSize*/}
+                    {/*    ? <Pagination size={"small"} count={5} page={props.item} onChange={onPageChangeHandler}/>*/}
+                    {/*    : <Pagination size={"small"} count={10} page={props.item} onChange={onPageChangeHandler}/>*/}
+                    {/*}*/}
+
+                    <Pagination size={"small"} count={props.count} page={props.item} onChange={onPageChangeHandler}/>
+                </div>
+                <div className={s.show}>
+                    <span>Show</span>
+                    <select onChange={onCardsPerPageChangeHandler}>
+                        <option value={5}>5</option>
+                        <option value={10} selected>
+                            10
+                        </option>
+                        <option value={15}>15</option>
+                    </select>
+                    <span>Cards per Page</span>
+                </div>
+            </div>
+        </div>
+    )
+})
