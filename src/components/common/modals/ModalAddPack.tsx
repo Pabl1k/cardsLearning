8ea7ce170f33e84b61a1c41804d276e4cafcca5e
PavlinMavlin <korobovpavel2017@yandex.by>
2021-08-12 13:09:57 +0300

    added modal

---
+++ src/components/common/modals/ModalAddPack.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
+import {ModalDelete} from "../modalWindow/modalDelete/modalDelete";
+import {ModalAdd} from "../modalWindow/modalAdd/modalAdd";
+
+function rand() {
+    return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyle() {
+    const top = 50 + rand();
+    const left = 50 + rand();
+
+    return {
+        top: `${top}%`,
+        left: `${left}%`,
+        transform: `translate(-${top}%, -${left}%)`,
+    };
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        paper: {
+            position: 'absolute',
+            width: 400,
+            backgroundColor: theme.palette.background.paper,
+            border: '2px solid #000',
+            boxShadow: theme.shadows[5],
+            padding: theme.spacing(2, 4, 3),
+        },
+    }),
+);
+type ModalAddPackPropsType={
+    addPack:(newValue:string)=>void
+    onCancelHandler:()=>void
+    onAddNewPackHandler:()=>void
+}
+export default function ModalAddPack(props:ModalAddPackPropsType) {
+    const classes = useStyles();
+    const [modalStyle] = React.useState(getModalStyle);
+
+    return (
+        <div style={modalStyle} className={classes.paper}>
+            <ModalAdd addPack={props.addPack} onCancelHandler={props.onCancelHandler} onAddNewPackHandler={props.onAddNewPackHandler}/>
+        </div>
+    );
+}
